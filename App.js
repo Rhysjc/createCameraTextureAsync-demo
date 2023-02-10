8e8d6d01d87e6764a36be5add03443297b12c3e3
Rhys Camm <rhysjc@gmail.com>
2023-02-10 20:34:10 +0000

    First commit

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { useRef, useEffect } from 'react';
+import { Camera, CameraType } from 'expo-camera';
+import { GLView } from 'expo-gl';
 
 export default function App() {
+  const cameraRef = useRef();
+  const glRef = useRef();
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+
+  useEffect(() => {
+    if (!permission) {
+      return;
+    }
+
+    if (!permission.granted) {
+      requestPermission();
+    }
+  }, [permission]);
+
+  const onContextCreate = async (gl) =>{
+    console.log('onContextCreate');
+    try {
+      await glRef.current.createCameraTextureAsync(cameraRef.current);
+
+      console.log('Got camera texture');
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  if (!permission) {
+    return null;
+  }
+
+  if (!permission.granted) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera style={styles.camera} type={CameraType.back} ref={cameraRef} />
+      <GLView style={styles.glView} ref={glRef} onContextCreate={onContextCreate} />
     </View>
   );
 }
@@ -17,4 +52,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  camera: {
+    width: 300,
+    height: 300,
+  },
+  glView: {
+    width: 300,
+    height: 300,
+  }
 });
